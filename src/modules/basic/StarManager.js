557006bc1ef2d70f181f6c58d73b416f16908acf
Kyostra <christopherahouse@gmail.com>
2021-06-16 02:57:30 -0500

    users dont have embed messages

--- src/modules/basic/StarManager.js
+++ src/modules/basic/StarManager.js
@@ -27,9 +27,9 @@ class StarManager extends Module {
 
         const imageURL = message.attachments[0] !== undefined ? imageType(message.attachments[0].url) : '';
 
-        // if (message.author.bot === true) {
-        //     return;
-        // }
+        if (message.author.bot === true) {
+            return;
+        }
 
         if (message.author.id === member) {
             this.send(`${message.channel.id}`, `${message.author.mention} You cannot star your own message 💢`);
@@ -47,59 +47,30 @@ class StarManager extends Module {
             return;
         }
 
-        if (message.embeds[0]) {
-            this.db.models.guilds.findOne({ serverID: message.channel.guild.id }, (error, g) => {
-                if (error) {
-                    this.logger.error('Error finding user in DB', error);
-                }
+        this.db.models.guilds.findOne({ serverID: message.channel.guild.id }, (error, g) => {
+            if (error) {
+                this.logger.error('Error finding user in DB', error);
+            }
 
-                if (!g || !g.starChannel) {
-                    return;
-                }
+            if (!g || !g.starChannel) {
+                return;
+            }
 
-                this.send(`${g.starChannel}`, `⭐ <#${message.channel.id}> (${message.id})`, { embed: {
-                    color: this._client.ryukoColor,
-                    author: {
-                        name: message.author.username,
-                        icon_url: message.author.dynamicAvatarURL()
-                    },
-                    description: `${message.embeds[0].author !== undefined ? message.embeds[0].author.name : ''}
-                    ${message.embeds[0].title !== undefined ? ' ' + message.embeds[0].title : ''}
-                    ${message.embeds[0].description !== undefined ? ' ' + message.embeds[0].description : ''}`,
-                    image: {
-                        url: `${message.embeds[0].image !== undefined ? message.embeds[0].image.url : ''}`
-                    },
-                    timestamp: new Date()
-                } }).then(() => {
-                    this.send(`${message.channel.id}`, `${message.author.mention} message (${message.id}) has been starred! ⭐`);
-                });
-            }).catch(this.logger.error);
-        } else if (!message.embeds[0]) {
-            this.db.models.guilds.findOne({ serverID: message.channel.guild.id }, (error, g) => {
-                if (error) {
-                    this.logger.error('Error finding user in DB', error);
-                }
-
-                if (!g || !g.starChannel) {
-                    return;
-                }
-
-                this.send(`${g.starChannel}`, `⭐ <#${message.channel.id}> (${message.id})`, { embed: {
-                    color: this._client.ryukoColor,
-                    author: {
-                        name: message.author.username,
-                        icon_url: message.author.dynamicAvatarURL()
-                    },
-                    description: message.content,
-                    image: {
-                        url: imageURL
-                    },
-                    timestamp: new Date()
-                } }).then(() => {
-                    this.send(`${message.channel.id}`, `${message.author.mention} message (${message.id}) has been starred! ⭐`);
-                });
-            }).catch(this.logger.error);
-        }
+            this.send(`${g.starChannel}`, `⭐ <#${message.channel.id}> (${message.id})`, { embed: {
+                color: this._client.ryukoColor,
+                author: {
+                    name: message.author.username,
+                    icon_url: message.author.dynamicAvatarURL()
+                },
+                description: message.content,
+                image: {
+                    url: imageURL
+                },
+                timestamp: new Date()
+            } }).then(() => {
+                this.send(`${message.channel.id}`, `${message.author.mention} message (${message.id}) has been starred! ⭐`);
+            });
+        }).catch(this.logger.error);
     }
 }
 
